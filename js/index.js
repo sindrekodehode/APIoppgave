385fdc255e6f318eddd6b939f9ef2279c6e40cca
sindrekodehode <sindrew.kodehode@gmail.com>
2023-08-21 14:54:25 +0200

    Begynner å komme noen vei

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
 const renderBtn = document.querySelector("button");
-const baseURL = "https://api.open5e.com/v1/monsters/";
+const baseURL = "https://api.open5e.com/monsters/?challenge_rating=1";
 const mainContainer = document.querySelector("main");
 const input = document.querySelector("#cr-input");
 
 let monster = [];
 
 renderBtn.addEventListener("click", async () => {
-  const data = await getData(getUrl(input.value));
-  renderMonster(data.results[0]);
+  const data = await getData(baseURL);
+  renderMonster(data[0]);
 });
 
 async function getData(url) {
   const request = await fetch(url);
   const data = await request.json();
-  monster = data;
-  return monster;
+  monster = data.results;
+  let filteredMonster = monster.filter(
+    (obj) => obj.cr.toString() === input.value
+  );
+  return filteredMonster;
 }
 
-// const monsterArray = monster;
+function RNG(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
-// function RNG(array) {
-//   return Math.floor(Math.random() * array.length);
+// function getUrl(num) {
+//   const monsterURL = "https://api.open5e.com/monsters/?challenge_rating=";
+//   const CR = num;
+//   const crUrl = `${monsterURL}${CR}`;
+//   return crUrl;
 // }
 
-function getUrl(num) {
-  const monsterURL = "https://api.open5e.com/monsters/?challenge_rating=";
-  const CR = num;
-  const crUrl = `${monsterURL}${CR}`;
-  return crUrl;
-}
-
 async function renderMonster(input) {
-  const renderMonster = await getData(getUrl(input.value));
+  const renderMonster = await getData(baseURL);
   const results = input;
 
   mainContainer.innerHTML = "";
