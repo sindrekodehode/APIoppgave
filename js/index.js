bbed7199fe86197141972669a0ad6bafef2dd238
sindrekodehode <sindrew.kodehode@gmail.com>
2023-08-22 11:38:06 +0200

    added filter again and refactored code

--- js/index.js
+++ js/index.js
@@ -1,38 +1,26 @@
 const renderBtn = document.querySelector("button");
-const baseURL = "https://api.open5e.com/monsters/?challenge_rating=1";
 const mainContainer = document.querySelector("main");
 const input = document.querySelector("#cr-input");
-
-let monster = [];
-
-renderBtn.addEventListener("click", async () => {
-  const data = await getData(baseURL);
-  renderMonster(data[0]);
-});
-
-async function getData(url) {
-  const request = await fetch(url);
-  const data = await request.json();
-  monster = data.results;
-  let filteredMonster = monster.filter(
-    (obj) => obj.cr.toString() === input.value
-  );
-  return filteredMonster;
-}
 
 function RNG(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-// function getUrl(num) {
-//   const monsterURL = "https://api.open5e.com/monsters/?challenge_rating=";
-//   const CR = num;
-//   const crUrl = `${monsterURL}${CR}`;
-//   return crUrl;
-// }
+function getUrl(cr) {
+  let filterUrl = `https://api.open5e.com/monsters/?limit=300&cr=${cr}`;
+  return filterUrl;
+}
+
+async function getData(url) {
+  const request = await fetch(url);
+  const data = await request.json();
+  let filteredMonster = data.results.filter(
+    (obj) => obj.cr.toString() === input.value
+  );
+  return filteredMonster;
+}
 
 async function renderMonster(input) {
-  const renderMonster = await getData(baseURL);
   const results = input;
 
   mainContainer.innerHTML = "";
@@ -41,3 +29,19 @@ async function renderMonster(input) {
   monsterElement.textContent = input.name;
   mainContainer.appendChild(monsterElement);
 }
+
+renderBtn.addEventListener("click", async () => {
+  console.log("clicked");
+  let url = getUrl(input.value);
+  const data = await getData(url);
+  let randomEntryNumber = RNG(data);
+  renderMonster(data[randomEntryNumber]);
+});
+
+input.addEventListener("keypress", async (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    renderBtn.click();
+  }
+});
