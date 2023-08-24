4d4ae9f4ec81f37ab1153d9a762c8acaf5946d9d
sindrekodehode <sindrew.kodehode@gmail.com>
2023-08-24 14:50:50 +0200

    Added more functionality to the "app"

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const renderBtn = document.querySelector("button");
+  const renderBtn = document.querySelector("#input-btn");
   const mainContainer = document.querySelector("main");
   const input = document.querySelector("#cr-input");
   const defaultMonster =
     "https://api.open5e.com/monsters/?desc__icontains=crab&cr=9";
+  const toTopBtn = document.querySelector("#to-top-btn");
+
+  toTopBtn.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 
   input.focus();
 
@@ -46,8 +51,74 @@ document.addEventListener("DOMContentLoaded", () => {
     <p>Cha:${input.charisma}</p>
   <br>
   </div>
+  <button id="monster-action-btn">Actions</button>
+  <button id="monster-saction-btn">Special Abilities</button>
   `;
+
+    const monsterActions = document.createElement("div");
+    monsterActions.id = "monster-actions-container";
+    monsterActions.classList.add("monster-actions-container");
+    monsterActions.classList.add("hidden");
+
+    monsterElement.appendChild(monsterActions);
+
+    input.actions.forEach((input, i) => {
+      const monsterActionElement = document.createElement("div");
+      monsterActionElement.classList.add(`monster-action-container-${i}`);
+
+      monsterActionElement.innerHTML = `
+      <h3>${input.name}</h3>
+      <p>${input.desc}</p>
+      `;
+      monsterActions.appendChild(monsterActionElement);
+    });
+
+    const monsterSActions = document.createElement("div");
+    monsterSActions.id = "monster-sactions-container";
+    monsterSActions.classList.add("monster-sactions-container");
+    monsterSActions.classList.add("hidden");
+
+    console.log();
+    monsterElement.appendChild(monsterSActions);
+
+    input.special_abilities.forEach((input, i) => {
+      const monsterSActionElement = document.createElement("div");
+      monsterSActionElement.classList.add(`monster-saction-container-${i}`);
+
+      monsterSActionElement.innerHTML = `
+      <h3>${input.name}</h3>
+      <p>${input.desc}</p>
+      `;
+
+      monsterSActions.appendChild(monsterSActionElement);
+    });
+
+    let monsterActionBtnClicked = true;
+    let monsterSActionBtnClicked = true;
+
     mainContainer.appendChild(monsterElement);
+
+    mainContainer.addEventListener("click", (event) => {
+      const target = event.target;
+      console.log("clicked");
+      if (target.id === "monster-action-btn") {
+        const monsterActionContainer = target
+          .closest(".monster-container")
+          .querySelector(".monster-actions-container");
+        if (monsterActionBtnClicked) {
+          monsterActionContainer.classList.toggle("hidden");
+        }
+        monsterActionBtnClicked = !monsterActionBtnClicked;
+      } else if (target.id === "monster-saction-btn") {
+        const monsterSActionContainer = target
+          .closest(".monster-container")
+          .querySelector(".monster-sactions-container");
+        if (monsterSActionBtnClicked) {
+          monsterSActionContainer.classList.toggle("hidden");
+        }
+        monsterSActionBtnClicked = !monsterSActionBtnClicked;
+      }
+    });
   }
 
   renderBtn.addEventListener("click", async () => {
